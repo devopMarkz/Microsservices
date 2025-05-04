71059ef3fad3d28c52c824867328f9d2266c7e72
devopMarkz <marcosdev2002@gmail.com>
2025-05-03 21:19:58 -0300

    Atualizando Flyway

--- erudio-microservices/cambio-service/src/main/java/br/com/erudio/cambio_service/repository/CambioRepository.java
+++ erudio-microservices/cambio-service/src/main/java/br/com/erudio/cambio_service/repository/CambioRepository.java
@@ -1,4 +1,14 @@
-package br.com.erudio.cambio_service;
+package br.com.erudio.cambio_service.repository;
 
-public interface CambioRepository {
+import br.com.erudio.cambio_service.model.Cambio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CambioRepository extends JpaRepository<Cambio, Long> {
+
+    Optional<Cambio> findByFromAndTo(String from, String to);
+
 }
