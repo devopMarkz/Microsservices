9c486826e7b5d372a5384151aaf82360f0fa5492
devopMarkz <marcosdev2002@gmail.com>
2025-07-19 02:10:15 -0300

    delecao de aarquivos legados

--- erudio-microservices/book-service/src/main/java/io/github/devopMarkz/book_service/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.devopMarkz.book_service.repository;
-
-import io.github.devopMarkz.book_service.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
