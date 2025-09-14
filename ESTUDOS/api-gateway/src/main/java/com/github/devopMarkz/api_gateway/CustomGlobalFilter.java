51f105ff69e0aa20d2f4511ec7893ef5602ec3bf
devopMarkz <marcosdev2002@gmail.com>
2025-09-14 15:59:48 -0300

    Interceptando filtro para adicionar secret no cabecalho de redirecionamento de requisisao

---
+++ ESTUDOS/api-gateway/src/main/java/com/github/devopMarkz/api_gateway/CustomGlobalFilter.java
@@ -0,0 +1,28 @@
+package com.github.devopMarkz.api_gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomGlobalFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        ServerHttpRequest mutatedRequest = request.mutate()
+                .header("X-Secret-Access", "my-secret")
+                .build();
+
+        ServerWebExchange mutatedExchange = exchange.mutate()
+                .request(mutatedRequest)
+                .build();
+
+        return chain.filter(mutatedExchange);
+    }
+
+}
